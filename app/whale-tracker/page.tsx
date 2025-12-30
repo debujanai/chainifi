579a28b649628a86f062595b1ccc7468115e9c96
debujanai <kashifjalal01@gmail.com>
2025-12-30 17:41:39 +0500

    whale tracker hide

--- app/whale-tracker/page.tsx
+++ app/whale-tracker/page.tsx
@@ -1,5 +1,10 @@
 import { WhaleTrackerBoard } from "@/components/whale-tracker-board";
+import { PageWrapper } from "@/components/page-wrapper";
 
 export default function WhaleTrackerPage() {
-    return <WhaleTrackerBoard />;
+    return (
+        <PageWrapper>
+            <WhaleTrackerBoard />
+        </PageWrapper>
+    );
 }
